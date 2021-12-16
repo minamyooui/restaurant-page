5d45a2f87947179cf56cbc9a5f72d91e9941a004
Marty <maruri2424@gmail.com>
2021-12-15 22:24:30 -0800

    minor css edits

--- src/contact.js
+++ src/contact.js
@@ -2,6 +2,8 @@ function loadContact() {
   const content = document.querySelector('#content');
   const h1 = document.createElement('h1');
   h1.textContent = 'Contact Us';
+  const div = document.createElement('div');
+  div.classList.toggle('contactInfo');
   const p0 = document.createElement('p');
   p0.textContent = '(951) 272-5894';
   p0.id = 'phoneNum';
@@ -10,9 +12,10 @@ function loadContact() {
   const p2 = document.createElement('p');
   p2.textContent = 'Corona, CA 92881';
   content.appendChild(h1);
-  content.appendChild(p0);
-  content.appendChild(p);
-  content.appendChild(p2);
+  content.appendChild(div);
+  div.appendChild(p0);
+  div.appendChild(p);
+  div.appendChild(p2);
 }
 
 export default loadContact;
