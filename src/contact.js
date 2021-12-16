b9a8ce35a404bca46a32fb6bf5cd80086ed65eaa
Marty <maruri2424@gmail.com>
2021-12-15 21:16:34 -0800

    fleshed out menu and contact pages

--- src/contact.js
+++ src/contact.js
@@ -2,7 +2,17 @@ function loadContact() {
   const content = document.querySelector('#content');
   const h1 = document.createElement('h1');
   h1.textContent = 'Contact Us';
+  const p0 = document.createElement('p');
+  p0.textContent = '(951) 272-5894';
+  p0.id = 'phoneNum';
+  const p = document.createElement('p');
+  p.textContent = '2630 Tuscany St #103'
+  const p2 = document.createElement('p');
+  p2.textContent = 'Corona, CA 92881';
   content.appendChild(h1);
+  content.appendChild(p0);
+  content.appendChild(p);
+  content.appendChild(p2);
 }
 
 export default loadContact;
--- src/menu.js
+++ src/menu.js
@@ -2,7 +2,22 @@ function loadMenu() {
   const content = document.querySelector('#content');
   const h1 = document.createElement('h1');
   h1.textContent = 'Menu';
+  const p = document.createElement('p');
+  p.id = 'menuLine';
+  p.textContent = '__________________________';
+  const ul = document.createElement('ul');
+  const i1 = document.createElement('li');
+  const i2 = document.createElement('li');
+  const i3 = document.createElement('li');
+  i1.textContent = 'Carpaccio';
+  i2.textContent =  'Bolognese';
+  i3.textContent = 'Carbonara';
+  ul.appendChild(i1);
+  ul.appendChild(i2);
+  ul.appendChild(i3);
   content.appendChild(h1);
+  content.appendChild(p);
+  content.appendChild(ul);
 }
 
 export default loadMenu;
